8d6054b854680be87c9a15b6f79d5f7ccc87b2cc
wzllby <441428735@qq.com>
2025-04-20 21:29:10 +0800

    添加银行回单识别功能

--- pohw/api/ocr.py
+++ pohw/api/ocr.py
@@ -8,3 +8,17 @@ def HouseholdRegister(file_path=None, file_url=None, sk=None, ak=None):
                   file_path=file_path,
                   file_url=file_url,
                   sk=sk, ak=ak)
+
+
+def SmartDocumentRecognizer(file_path=None, file_url=None, sk=None, ak=None):
+    return do_api(OCR_NAME=str(sys._getframe().f_code.co_name),
+                  file_path=file_path,
+                  file_url=file_url,
+                  sk=sk, ak=ak)
+
+
+def BankReceipt(file_path=None, file_url=None, sk=None, ak=None):
+    return do_api(OCR_NAME=str(sys._getframe().f_code.co_name),
+                  file_path=file_path,
+                  file_url=file_url,
+                  sk=sk, ak=ak)
--- tests/test_code/test_ocr2excel.py
+++ tests/test_code/test_ocr2excel.py
@@ -17,9 +17,16 @@ class Ocr2Excel(unittest.TestCase):
                                 sk=self.CLOUD_SDK_SK)
         self.assertTrue(os.path.exists('../test_files/HouseholdRegister/HouseholdRegister2Excel.xlsx'))
         os.remove('../test_files/HouseholdRegister/HouseholdRegister2Excel.xlsx')
-        HouseholdRegister2Excel(file_url='https://p8.itc.cn/q_70/images01/20230704/3dcd14733b3640c49aa1bdabc2c0c9fd.jpeg',
-                                output_path='../test_files/HouseholdRegister',
-                                ak=self.CLOUD_SDK_AK,
-                                sk=self.CLOUD_SDK_SK)
+        HouseholdRegister2Excel(
+            file_url='https://p8.itc.cn/q_70/images01/20230704/3dcd14733b3640c49aa1bdabc2c0c9fd.jpeg',
+            output_path='../test_files/HouseholdRegister',
+            ak=self.CLOUD_SDK_AK,
+            sk=self.CLOUD_SDK_SK)
         self.assertTrue(os.path.exists('../test_files/HouseholdRegister/HouseholdRegister2Excel.xlsx'))
         os.remove('../test_files/HouseholdRegister/HouseholdRegister2Excel.xlsx')
+
+    def test_BankReceipt2excel(self):
+        BankReceipt2excel(input_path='../test_files/BankReceipt/img.png',
+                          output_path='../test_files/BankReceipt',
+                          ak='HPUAF9VVGS4MXXSRA6GS',
+                          sk='zPRCzv4jRFcY29FPCwAPdGFX74xIOjDm2bbwMfDm')
