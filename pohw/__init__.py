2f9ad1e6304c67503d64ccedc2e0b744605b3730
CoderWanFeng <1957875073@qq.com>
2026-03-02 23:55:24 +0800

    update

--- pohw/__init__.py
+++ pohw/__init__.py
@@ -5,8 +5,8 @@ __doc__ = """
 【poocr库】，功能持续更新中
 使用有问题 or 提交你的功能需求 or 参与项目开发
 1、全部功能【视频 & 文字】教程：https://www.python-office.com/course-002/5-poocr/5-poocr.html
-2、请+【项目交流群】：http://www.python4office.cn/wechat-group/
-3、本开源项目的【代码仓库】：https://gitcode.com/python4office/poocr
+2、请+【项目交流群】：https://www.python4office.cn/wechat-group/
+3、本开源项目的【代码仓库】：https://atomgit.com/python4office/poocr
 4、给小白的【50讲Python自动化办公】：https://www.python-office.com/course/50-python-office.html
 """
 
